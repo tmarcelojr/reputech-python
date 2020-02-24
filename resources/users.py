f5ca4e0e834c727fefd4bac5f15252dca283bb1c
Teo Marcelo <tmarcelojr@gmail.com>
2020-02-24 15:25:02 -0600

    users can login

--- resources/users.py
+++ resources/users.py
@@ -50,6 +50,37 @@ def register():
 				status=201
 			), 201
 
+# Login
+@users.route('/login', methods=['POST'])
+def login():
+	payload = request.get_json()
+	payload['username'] = payload['username'].lower()
+	try:
+		user = User.get(User.username == payload['username'])
+		user_dict = model_to_dict(user)
+		password_is_good = check_password_hash(user_dict['password'], payload['password'])
+		if password_is_good:
+			login_user(user)
+			user_dict.pop('password')
+			return jsonify(
+					data=user_dict,
+	  			message=f"Successfully logged in as {user_dict['username']}",
+	  			status=200
+  			), 200
+		else:
+  		# This means password is not correct.
+			return jsonify(
+      	data={},
+        message="Username or password is incorrect",
+        status=401
+      	), 401
+	except DoesNotExist:
+	# Username not correct
+		return jsonify(
+        data={},
+        message="Username or password is incorrect",
+        status=401
+      ), 401
 
 
 
