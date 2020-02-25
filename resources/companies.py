ed760c7f97b0633a095dfcfe3c25604f02aac831
Teo Marcelo <tmarcelojr@gmail.com>
2020-02-25 11:24:26 -0600

    can add companies, index page rendering all added companies

--- resources/companies.py
+++ resources/companies.py
@@ -17,7 +17,23 @@ companies = Blueprint('companies', 'companies')
 def reviews_index():
 	companies_dicts = [model_to_dict(company) for company in Company]
 	return jsonify(
-			data=companies_dicts,
-			message=f'Successfully retrieved {len(companies_dicts)} companies',
-			status=200
-		), 200
+		data=companies_dicts,
+		message=f'Successfully retrieved {len(companies_dicts)} companies',
+		status=200
+	), 200
+
+# Add Company
+@companies.route('/', methods=['POST'])
+def add_company():
+	payload = request.get_json()
+	company = Company.create(
+		name=payload['name'],
+		website=payload['website']
+	)
+
+	company_dict = model_to_dict(company)
+	return jsonify(
+		data=company_dict,
+		message=f"Successfully added company {payload['name']}.",
+		status=201
+	), 201
