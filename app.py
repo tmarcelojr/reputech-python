e9efc2c9c21e39e177c7fd2579ad44ac80b54310
Teo Marcelo <tmarcelojr@gmail.com>
2021-02-24 23:50:21 -0600

    added resources params on cors

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ DEBUG = True
 PORT = 8000
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
-CORS(app, origins=['http://localhost:3000', 'https://reputech-chicago.herokuapp.com', 'https://reputech-chicago.web.app'], supports_credentials=True)
+CORS(app, resources=r'/api/*', origins=['http://localhost:3000', 'https://reputech-chicago.herokuapp.com', 'https://reputech-chicago.web.app'], supports_credentials=True)
 
 # ==============================
 # 				LOGIN MANAGER
