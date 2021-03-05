f118bcce460dc45677f245ff051b63db964731f4
Teo Marcelo <tmarcelojr@gmail.com>
2021-03-05 15:10:57 -0600

    added simple session

--- resources/users.py
+++ resources/users.py
@@ -140,21 +140,11 @@ def delete_user(id):
 # Check current user
 @users.route('/logged_in', methods=['GET'])
 def get_logged_in_user():
-	if not current_user.is_authenticated:
+	if 'username' in session:
+		user = session=['username']
 		return jsonify(
-			data={},
-			message='No user is currently logged in',
-			status=401
-			), 401
-		
-	else:
-		user_dict = model_to_dict(current_user)
-		user_dict.pop('password')
-		return jsonify(
-			data=user_dict,
-			message=f"Current user is {user_dict['username']}", 
-			status=200
-		), 200
+			data=user
+		)
 
 
 
